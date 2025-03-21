f9840a3a2bb010b302419a937c5a3f5d39580170
devDaal <diegoaloz98993@gmail.com>
2025-03-21 13:58:11 -0600

    I added the modules for the commands' constants and the questions for the next features are in the homing_routine module in the send_index... method

---
+++ Commands/leitz_protocol.py
@@ -0,0 +1,6 @@
+baudrate = 9600
+timeout = 1
+home_x = 1
+home_y = 2
+home_z = 3
+home_all = 9
--- models/homing_routine.py
+++ models/homing_routine.py
@@ -11,6 +11,13 @@ class Homing_Routine(ObservableModel):
         #variables
         
     def send_index_to_serial_manager(self,axis):
+        
+        #Aquí hay que cambiar esto por tomar el valor de la selección de protocolo 
+        #y ver a dónde enviarlo. Aquí hay que recibir la selección del protocolo del usuario
+        #decidir de dónde vamos a importar los comandos, regresar la información al controlador
+        # y enviarla a serial_manager. 
+        # ¿El serial manager debe tener la funcionalidad de los 4 protocolos? o debe de ser un nuevo modelo?
+        # ¿Cómo comunico a los modelos? ¿Cómo envío los comandos y la rutina de cada protocolo a serial_manager?
         self.axis_options = ('x','y','z',)
         if axis in self.axis_options:
             self.axis_option = self.axis_options.index(axis,0,3) + 1
